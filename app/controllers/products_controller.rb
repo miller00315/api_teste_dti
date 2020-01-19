ee688157d5f8240d3d1a00f2ea195a7e7eb149ee
Miller Oliveira <miller00315@gmail.com>
2020-01-19 13:34:38 +0000

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,56 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all.order(:name)
+    render json: @products
+  end
+ 
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: product.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def show
+    if @product.present?
+      render json: @product
+    else
+      render status: :not_found
+    end
+  end
+
+  def update
+    if @product.present?
+      @product.update_attributes(product_params)
+      render json: @product
+   else
+      render status: :not_found
+    end	
+  end
+
+  def destroy
+    if @product.present?
+      @product.destroy
+      render json: @product
+    else 
+      render status: :not_found
+    end
+  end
+
+  private
+
+    def set_product
+      if Product.exists?(params[:id])
+      	@product = Product.find(params[:id])
+      end
+    end
+
+    def product_params
+	params.require(:product).permit(:name, :quantity, :value)
+    end
+end
